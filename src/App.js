29bf13693b71b810d560e52c7bf64aa3b967d83a
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2024-10-21 16:20:13 +0100

    First commit of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Heading() {
+  return (
+    <h1>Date Counter</h1>
+  )
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  // let message = "";
+
+  let today = new Date();
+  today.setDate(today.getDate() + count)
+  let displayDate = today.toDateString()
+ 
+
+  // today.setDate(today.getDate() + 0)
+
+  function incrementStep() {
+    setStep((s) => s+1)
+  }
+  
+  function decrementStep() {
+    setStep((s) => s-1);
+  }
+
+  function increaseCount() {
+    setCount((c) => c + step);
+  }
+
+  function decreaseCount() {
+    setCount((c) => c - step);
+  }
+
+  // if (count === 0) message = `Today is ${displayDate}`;
+  // if (count === 1) message = `Tomorrow is ${displayDate}`
+  // if (count > 1) message = `${count} days from today is ${displayDate}`
+
+
+  // if (count === -1) message = `Yesterday was ${displayDate}` 
+  // if (count < -1) message = `${Math.abs(count)} days ago was ${displayDate}`
+
+  const messages = {
+    0: `Today is ${displayDate}`,
+    1: `Tomorrow is ${displayDate}`,
+    '-1': `Yesterday was ${displayDate}`
+};
+
+  let message = messages[count] || 
+    (count > 1 
+        ? `${count} days from today is ${displayDate}` 
+        : `${Math.abs(count)} days ago was ${displayDate}`);
+
+  return (
+    <div className="container">
+      <div className="btn-container">
+        <button className="button" onClick={decrementStep}>
+          &#8722;
+        </button>
+        <span>Step: {step}</span>
+        <button className="button" onClick={incrementStep}>
+          &#43;
+        </button>
+      </div>
+      <div className="btn-container">
+        <button className="button" onClick={decreaseCount}>
+          &#8722;
+        </button>
+        <span>Count: {count}</span>
+        <button className="button" onClick={increaseCount}>
+          &#43;
+        </button>
+      </div>
+      <p className="date">{message}</p>
+    </div>
+  );
+}
